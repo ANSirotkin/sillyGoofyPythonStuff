17cd38a862e512eb976032ffd52b1a4193a20803
ANSirotkin <ansirotkin@icloud.com>
2022-11-07 20:23:30 -0500

    mapGen improved (more random shapes)

--- mapGen.py
+++ mapGen.py
@@ -4,32 +4,29 @@ import random as rd
 import math
 
 def genShape(board, sNum):
-    while True:
         x, y = rd.randint(0, int(len(board) - 2 - math.ceil(sNum/2))), rd.randint(0, int(len(board[0]) - math.ceil(sNum/2)))
-        free = True
+        free = False
         locarr = []
         for i in range(sNum):
             if i % rd.randint(2, 3) == 0:
                 X = x + 1
             else: X = x
             Y = y + int(i / 2)
-            if (board[X][Y] != " "):
-                free = False
+            if (board[X][Y] == " " or board[X][Y] == "#"):
+                free = True
             locarr.append([X, Y])
         if free:
             for loc in locarr:
                 board[loc[0]][loc[1]] = "#"
-            break
-        break
 
 def genMap():
-    board = np.empty([20, 30]).astype(np.str_)
+    board = np.empty([20, 40]).astype(np.str_)
 
     for x in range(len(board)):
         for y in range(len(board[0])):
             board[x][y] = " "
 
-    for x in range(50):
+    for x in range(30):
         sNum = rd.randint(1, len(board) - 1)
         genShape(board, sNum)
 
