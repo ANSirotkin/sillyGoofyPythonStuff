d8581bd2f6c6f4bb848572d247957841117ed685
ANSirotkin <ansirotkin@icloud.com>
2022-11-07 19:57:26 -0500

    made mapgen better

--- mapGen.py
+++ mapGen.py
@@ -5,11 +5,11 @@ import math
 
 def genShape(board, sNum):
     while True:
-        x, y = rd.randint(0, int(len(board) - 1 - math.ceil(sNum/2))), rd.randint(0, int(len(board[0]) - math.ceil(sNum/2)))
+        x, y = rd.randint(0, int(len(board) - 2 - math.ceil(sNum/2))), rd.randint(0, int(len(board[0]) - math.ceil(sNum/2)))
         free = True
         locarr = []
         for i in range(sNum):
-            if i % 2 == 0:
+            if i % rd.randint(2, 3) == 0:
                 X = x + 1
             else: X = x
             Y = y + int(i / 2)
@@ -23,15 +23,29 @@ def genShape(board, sNum):
         break
 
 def genMap():
-    board = np.empty([15, 15])
-    board = board.astype(np.str_)
+    board = np.empty([20, 30]).astype(np.str_)
 
     for x in range(len(board)):
         for y in range(len(board[0])):
             board[x][y] = " "
 
-    for x in range(20):
+    for x in range(50):
         sNum = rd.randint(1, len(board) - 1)
         genShape(board, sNum)
 
     return board
+
+def mapTester(iter):
+    for x in range(iter):
+        try:
+            genMap()
+        except:
+            print("uh oh")
+
+# mapTester(100)
+
+map = genMap()
+for x in map:
+    for y in x:
+        print(str(y) + " ", end="")
+    print()
