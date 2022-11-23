b1145272fef5b19d56115c3b034fd985acaca4c5
ANSirotkin <ansirotkin@icloud.com>
2022-11-23 12:53:35 -0500

    Create gorgeousSoup.py

---
+++ gorgeousSoup.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://cnn.com/")
+if response.status_code != 200:
+	print("Error fetching page")
+	exit()
+else:
+	content = response.text
+
+soup = BeautifulSoup(response.content, 'html.parser')
